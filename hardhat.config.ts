5d4632c4126b97b282d09f5fb711b9e6fdecfc85
cephasjoe <cephasjoe@gmail.com>
2022-03-12 04:44:40 -0500

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,50 @@
+import dotenv from "dotenv";
+dotenv.config(); // load env vars from .env
+import { task, HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "./tasks/index";
+
+const { ARCHIVE_URL } = process.env;
+
+if (!ARCHIVE_URL)
+  throw new Error(
+    `ARCHIVE_URL env var not set. Copy .env.template to .env and set the env var`
+  );
+
+const accounts = {
+  // use default accounts
+  mnemonic: `test test test test test test test test test test test junk`,
+};
+
+// Go to https://hardhat.org/config/ to learn more
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      // add missing versions here
+      { version: "0.4.11" },
+      { version: "0.6.2" },
+      { version: "0.7.0" },
+      { version: "0.8.0" },
+    ],
+  },
+  networks: {
+    hardhat: {
+      accounts,
+      loggingEnabled: false,
+      forking: {
+        url: ARCHIVE_URL, // https://eth-mainnet.alchemyapi.io/v2/SECRET`,
+        blockNumber: 11940499,
+      },
+    },
+    local: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  mocha: {
+    timeout: 300 * 1e3,
+  },
+
+};
+
+export default config;
